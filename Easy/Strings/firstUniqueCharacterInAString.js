441a9ce6b8589c9a138edbc5cf8af01513f13c8b
Michaela-K <michaelakodes@gmail.com>
2022-09-01 20:00:00 -0400

    pseudocode

--- Easy/Strings/firstUniqueCharacterInAString.js
+++ Easy/Strings/firstUniqueCharacterInAString.js
@@ -15,7 +15,11 @@
 // Output: -1
 
 const firstUniqChar = function(s) {
-
+  //loop over string
+  //take first element(make it a variable)
+  //compare first element with all the rest
+  //if no equal thats the answer
+  //if equal, then take next index as element
 };
 
 console.log(firstUniqChar("leetcode"))
